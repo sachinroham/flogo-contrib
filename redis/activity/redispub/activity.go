bd8a1971778ba7428713ac5a23a1a38934c5b028
sachinroham <37993963+sachinroham@users.noreply.github.com>
2018-04-04 20:03:58 +0800

    added logic

---
+++ redis/activity/redispub/activity.go
@@ -0,0 +1,64 @@
+package redispub
+
+import (
+	"strconv"
+"fmt"
+	"github.com/go-redis/redis"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("activity-redis-pub")
+
+const (
+	ivHost     = "host"
+	ivPort     = "port"
+	ivPassword = "password"
+	ivChannel  = "channel"
+	ivMessage  = "message"
+	ovResult   = "result"
+)
+
+// REDISPUBActivity is an Activity is used to store a file on FTP server
+// inputs : {host,port,user,password,path,content}
+// outputs: {result}
+type REDISPUBActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new RESTActivity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &REDISPUBActivity{metadata: metadata}
+}
+
+// Metadata returns the activity's metadata
+func (a *REDISPUBActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements api.Activity.Eval - Invokes a LISTFILE Operation
+func (a *REDISPUBActivity) Eval(context activity.Context) (done bool, err error) {
+
+	host := context.GetInput(ivHost).(string)
+	port := context.GetInput(ivPort).(int)
+	password := context.GetInput(ivPassword).(string)
+	channel := context.GetInput(ivChannel).(string)
+	message := context.GetInput(ivMessage).(string)
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     host + ":" + strconv.Itoa(port),
+		Password: "" + password, // no password set
+		DB:       0,             // use default DB
+	})
+	
+	fmt.Println("Data ", host + ":" + strconv.Itoa(port))
+	resp, err := client.Publish(channel, message).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	context.SetOutput(ovResult, resp)
+	return true, nil
+}
---
+++ redis/activity/redispub/activity_test.go
@@ -0,0 +1,65 @@
+package redispub
+
+import (
+	"fmt"
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json yes Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+var host = "sroham-t430"
+var port = 6379
+var password = ""
+var channel = "redisChat"
+var message = "Redis is a great caching technique"
+
+
+func TestListFiles(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("host", host)
+	tc.SetInput("port", port)
+	tc.SetInput("password", password)
+	tc.SetInput("channel", channel)
+	tc.SetInput("message", message)
+	fmt.Println(strings.Join([]string{host, strconv.Itoa(21)}, ":"))
+	//eval
+	act.Eval(tc)
+	val := tc.GetOutput("result")
+
+	fmt.Printf("result: %v\n", val)
+
+}
